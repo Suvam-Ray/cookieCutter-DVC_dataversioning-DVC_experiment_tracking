550e0d25da59e40093f1a189b2fb87adf04b3dd2
Suvam Ray <suvamray2008@gmail.com>
2025-01-17 19:00:36 +0530

    MLFlow exp tracker step 1 : Modified code in src\models\train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,17 +1,15 @@
 import random
 import sys
-from dvclive import Live
 import yaml
 from pathlib import Path
+import mlflow
 
-
-with Live(save_dvc_exp=True) as live:
+with mlflow.start_run():
     train_params = yaml.safe_load(open(Path(__file__).resolve().parent.parent.parent / 'params.yaml'))['train']
     epochs = train_params['epochs']
-    live.log_param("epochs", epochs)
+    mlflow.log_param("epochs", epochs)
     for epoch in range(epochs):
-        live.log_metric("train/accuracy", epoch + random.random())
-        live.log_metric("train/loss", epochs - epoch - random.random())
-        live.log_metric("val/accuracy",epoch + random.random() )
-        live.log_metric("val/loss", epochs - epoch - random.random())
-        live.next_step()
+        mlflow.log_metric("train/accuracy", epoch + random.random())
+        mlflow.log_metric("train/loss", epochs - epoch - random.random())
+        mlflow.log_metric("val/accuracy",epoch + random.random() )
+        mlflow.log_metric("val/loss", epochs - epoch - random.random())
